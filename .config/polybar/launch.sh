768ad7bfba4b31ce9daa7816989436a3b72a0f20
JustineSmithies <justine@smithies.me.uk>
2021-08-23 18:08:12 +0100

    Correcting config errors

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,8 +5,10 @@ killall -q polybar
 # If all your bars have ipc enabled, you can also use 
 # polybar-msg cmd quit
 
+while pgrep  -u $UID -x polybar >/dev/null; do sleep 1; done
+
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+echo "---" | tee -a /tmp/polybar1.log
 polybar top 2>&1 | tee -a /tmp/polybar1.log & disown
 
 echo "Bars launched..."
