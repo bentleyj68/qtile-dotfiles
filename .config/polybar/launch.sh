02db73feb3a6426408f52c6376ae75d9ca780dda
JustineSmithies <justine@smithies.me.uk>
2021-08-24 21:17:46 +0100

    Added date / time stamp to log file when Polybar starts

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,7 +8,7 @@ killall -q polybar
 while pgrep  -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log
+echo "---> $(date)" | tee -a /tmp/polybar1.log
 polybar top 2>&1 | tee -a /tmp/polybar1.log & disown
 
 echo "Bars launched..."
