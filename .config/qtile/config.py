6566bb264d0b585e4bcb79b62c50b3859b2dd224
JustineSmithies <justine@smithies.me.uk>
2021-10-01 22:14:12 +0100

    Added ability to set each groups layout individually in config.py

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -356,21 +356,22 @@ keys = [
 # Groups with matches
 
 workspaces = [
-    {"name": " ₁", "key": "1", "matches": [Match(wm_class='firefox')]},
-    {"name": " ₂", "key": "2", "matches": [Match(wm_class='kitty'), Match(wm_class='ranger')]},
-    {"name": " ₃", "key": "3", "matches": [Match(wm_class='vim')]},
-    {"name": " ₄", "key": "4", "matches": [Match(wm_class='telegram-desktop'), Match(wm_class='weechat')]},
-    {"name": " ₅", "key": "5", "matches": [Match(wm_class='gimp-2.10')]},
-    {"name": "阮 ₆", "key": "6", "matches": [Match(wm_class='spotify')]},
-    {"name": " ₇", "key": "7", "matches": [Match(wm_class='libreoffice')]},
-    {"name": " ₈", "key": "8", "matches": [Match(wm_class='newsboat')]},
-    {"name": " ₉", "key": "9", "matches": [Match(wm_class='neomutt')]},
+    {"name": " ₁", "key": "1", "matches": [Match(wm_class='firefox')], "layout": "monadtall"},
+    {"name": " ₂", "key": "2", "matches": [Match(wm_class='kitty'), Match(wm_class='ranger')], "layout": "monadtall"},
+    {"name": " ₃", "key": "3", "matches": [Match(wm_class='vim')], "layout": "monadtall"},
+    {"name": " ₄", "key": "4", "matches": [Match(wm_class='telegram-desktop'), Match(wm_class='weechat')], "layout": "monadtall"},
+    {"name": " ₅", "key": "5", "matches": [Match(wm_class='gimp-2.10')], "layout": "monadtall"},
+    {"name": "阮 ₆", "key": "6", "matches": [Match(wm_class='spotify')], "layout": "monadtall"},
+    {"name": " ₇", "key": "7", "matches": [Match(wm_class='libreoffice')], "layout": "monadtall"},
+    {"name": " ₈", "key": "8", "matches": [Match(wm_class='newsboat')], "layout": "monadtall"},
+    {"name": " ₉", "key": "9", "matches": [Match(wm_class='neomutt')], "layout": "monadtall"},
 ]
 
 groups = []
 for workspace in workspaces:
     matches = workspace["matches"] if "matches" in workspace else None
-    groups.append(Group(workspace["name"], matches=matches, layout="monadtall"))
+    layouts = workspace["layout"] if "layout" in workspace else None
+    groups.append(Group(workspace["name"], matches=matches, layout=layouts))
     keys.append(Key([mod], workspace["key"], lazy.group[workspace["name"]].toscreen()))
     keys.append(Key([mod, "shift"], workspace["key"], lazy.window.togroup(workspace["name"])))
 
